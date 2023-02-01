9acadcf94e05096411cf5915ee52fbf5c3a51894
berkut3nko <ldproggramer112@gmail.com>
2023-02-01 20:06:52 +0200

    0.0.0.2.2

--- Assets/Scripts/AgentControl.cs
+++ Assets/Scripts/AgentControl.cs
@@ -5,7 +5,8 @@ public class AgentControl : MonoBehaviour
 {
     private NavMeshAgent _agent;
     private Camera _camera;
-    private RaycastHit tempHit;
+    RaycastHit tempHit;
+    bool goPlant=false;
     void Start()
     {
         _camera = Camera.main;
@@ -15,12 +16,33 @@ public class AgentControl : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0) && !goPlant)//если идет сажать не сбивать направление
         {
             if(Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition),out tempHit))
             {
                 _agent.SetDestination(tempHit.point);
             }
         }
+        else
+        if (Input.GetMouseButtonDown(1) && !goPlant)//посадить в указаной точке
+        {
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out tempHit))
+            {
+                _agent.SetDestination(tempHit.point);
+                goPlant = true;
+            }
+        }
+        else if((Input.GetMouseButtonDown(1) || Input.GetMouseButton(0)) && goPlant)//Отмена посадки
+        {
+            _agent.SetDestination(transform.position);//Зупинитися
+            goPlant=false;
+        }
+        if(goPlant && (_agent.destination-transform.position).magnitude <= 1)
+        {
+            //Анимация саджання и код
+            Debug.Log("Plant");
+            goPlant = false;
+        }
+
     }
 }
